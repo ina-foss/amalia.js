9ac47385d3d4bb772894d86a90ac9c81e68048a0
sdrn <srakulan@gmail.com>
2016-06-21 09:32:21 +0200

    dist v 1.3.2

---
+++ dist/js/i18n/amalia.js-message-en.js
@@ -0,0 +1,27 @@
+/*! V1.3.2, © INA 2016 */
+/**
+ * Copyright (c) 2015 Institut National de l'Audiovisuel, INA
+ * This file is part of amalia.js
+
+ * amalia.js is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+ * Redistributions of source code, javascript and css minified versions
+ * must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+ * Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+
+ * You should have received a copy of the GNU General Public License
+ * along with amalia.js. If not, see <http://www.gnu.org/licenses/>
+
+ *
+ * amalia.js is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+fr.ina.amalia.player.PlayerErrorMessage.ACCESS_DENIED="Access denied.",fr.ina.amalia.player.PlayerErrorMessage.MEDIA_FILE_NOT_FOUND="Error Loading Media.",fr.ina.amalia.player.PlayerErrorMessage.EXCEPTION="An exception occurred.",fr.ina.amalia.player.PlayerErrorMessage.HTTP_ERROR="Http response at 400 or 500 level.",fr.ina.amalia.player.PlayerErrorMessage.ABORT="Your request has been interrupted.",fr.ina.amalia.player.PlayerErrorMessage.TIMEOUT="Timeout.",fr.ina.amalia.player.PlayerErrorMessage.ERROR_LOAD_PLUGIN="Error during plugin initialization.",fr.ina.amalia.player.PlayerErrorMessage.CUSTOM_ERROR="An error has occurred on your player.",fr.ina.amalia.player.PlayerErrorMessage.ERROR="Error has occurred please try again later.",fr.ina.amalia.player.PlayerErrorMessage.DEFAULT="Error has occurred please try again later.",fr.ina.amalia.player.PlayerErrorMessage.ERROR_HTML5_SUPPORT="Error loading player: HTML5 video not supported",fr.ina.amalia.player.PlayerErrorMessage.ERROR_MANIFEST_DASH="Error to load manifest dash",fr.ina.amalia.player.PlayerMessage.PLAYER_OPENSOURCE_LINK="Available as open source.",fr.ina.amalia.player.PlayerMessage.PLUGIN_OVERLAY_CONTEXT_MENU_ENABLED_DISABLED_PLUGIN="Enable or disable plugin overlay.",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_ELEMENTS="items.",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_ZOOM_IN="Zoom in",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_ZOOM_OUT="Zoom out",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_CHANGE_DISPLAY="Switch lines height",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_SLIDE_LEFT="Slide Left",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_SLIDE_RIGHT="Slide Right",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_SCROLL_UP="Scroll Up",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_SCROLL_DOWN="Scroll Down",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_SHOW_HIDE_TOOTIP="Enable/disable the help tooltip",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_EXPAND="Expand",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_SORT="Sort",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_NAV_POINT_PREV="Go to previous marker",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_NAV_POINT_NEXT="Go to next marker",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_TIMEAXIS="Time axis",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_CLOSE="Close",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_LIST_EDITOR_LABEL_HEADER="Metadata list editor",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_LABEL_HEADER="Metadata block editor",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_ITEMS_EDITOR_LABEL_HEADER="Metadata items editor",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_ID="Id",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_LABAL="Label",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_TYPE="Type",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_DESCRIPTION="Description",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_REFERENCE="Reference",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_AUTHOR="Author",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_SHAPE="Shape",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_BLOCK_EDITOR_FORM_LABEL_METADATA_COLOR="Color",fr.ina.amalia.player.PlayerMessage.PLUGIN_METADATA_ITEMS_EDITOR_NEED_METADTA="You need to select metadata item type.",fr.ina.amalia.player.PlayerMessage.PLUGIN_TIMELINE_LABEL_BIND="Bind/Unbind",fr.ina.amalia.player.PlayerMessage.PLUGIN_D3JS_CHART_LABELS=["Grouped","Stacked"];
